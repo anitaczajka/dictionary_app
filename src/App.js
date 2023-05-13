bdd89a179039f5d590dca09a501bb504b12cf657
Anita <anita.czajka.91@outlook.com>
2023-05-13 22:28:07 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Dictionary App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>Dictionary App</h2>
+        <button className="btn btn-primary">Search</button>
+      </div>
+    </div>
+  );
 }
