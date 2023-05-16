566335b8cbff34474a70ee2632d922facd216075
Anita <anita.czajka.91@outlook.com>
2023-05-16 14:50:46 +0200

    created logo for the application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import logo from "./images/logo.png";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h2>Dictionary App</h2>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
         <Dictionary />
       </div>
     </div>
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Meaning(props) {
-  console.log(props.meaning.definition);
+  console.log(props.meaning.definition[0]);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
