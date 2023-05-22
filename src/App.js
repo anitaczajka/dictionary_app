4d8c21279b57a73f03ecb3bb68cb31152f7fbbab
Anita <anita.czajka.91@outlook.com>
2023-05-22 17:57:29 +0200

    styled the application

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
-        <Dictionary />
+        <Dictionary defaultKeyword="hello" />
         <footer className="App-footer mt-4">Coded by Anita Czajka</footer>
       </div>
     </div>
